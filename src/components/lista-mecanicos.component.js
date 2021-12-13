941bd7f33991e02505efa873fe211691c7c6e71b
JeimmyDeAguasSotelo <jsotelo@uninorte.edu.co>
2021-12-12 22:08:29 -0500

    filtro mecanicos

--- src/components/lista-mecanicos.component.js
+++ src/components/lista-mecanicos.component.js
@@ -2,22 +2,51 @@ import React, { Component } from "react";
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import UsuarioTableRow from './UsuarioTableRow';
-
+import Form from 'react-bootstrap/Form'
 
 export default class ListaUsuarios extends Component {
 
   constructor(props) {
     super(props)
+
+    this.onChangeTipoFiltro = this.onChangeTipoFiltro.bind(this);
+    this.onChangeCadenaFiltro = this.onChangeCadenaFiltro.bind(this);
+
     this.state = {
-      usuarios: []
+      textobuscar:'',
+      filtrobuscar:'nombre',
+      usuarios: [],
+      filtrados: []
     };
+  }
+
+  ejecutaFiltro(cadena, filtro){
+    if(cadena === ''){
+        this.setState({ filtrados: this.state.usuarios });
+        return;
+    }            
+    this.setState({ filtrados: this.state.usuarios.filter(function (el) {
+      return el[filtro].toString().toLowerCase().indexOf(cadena.toString().toLowerCase()) > -1;
+    }) });            
+   
+  }
+
+  onChangeCadenaFiltro(e) {
+
+    this.setState({ textobuscar: e.target.value });
+    this.ejecutaFiltro(e.target.value, this.state.filtrobuscar)
+  }
+  onChangeTipoFiltro(e) {
+      this.setState({ filtrobuscar: e.target.value });
+      this.ejecutaFiltro(this.state.textobuscar, e.target.value)
   }
 
   componentDidMount() {
     axios.get('http://localhost:4000/usuarios/mecanicos/')
       .then(res => {
         this.setState({
-          usuarios: res.data
+          usuarios: res.data,
+          filtrados: res.data
         });
       })
       .catch((error) => {
@@ -26,7 +55,7 @@ export default class ListaUsuarios extends Component {
   }
 
   DataTable() {
-    return this.state.usuarios.map((res, i) => {
+    return this.state.filtrados.map((res, i) => {
       return <UsuarioTableRow obj={res} key={i} />;
     });
   }
@@ -34,6 +63,27 @@ export default class ListaUsuarios extends Component {
 
   render() {
     return (<div>
+      <Table striped bordered hover>
+            <tbody>
+                <tr>
+                    <td>
+                        <Form.Group controlId="Descripcion">
+                            <Form.Control placeholder="Buscar" type="text" value={this.state.textobuscar} onChange={this.onChangeCadenaFiltro} />
+                        </Form.Group>
+                    </td>
+                    <td>
+                        <Form.Control as="select" value={this.state.filtrobuscar} onChange={this.onChangeTipoFiltro} >
+                            <option value="nombre">Nombre</option>
+                            <option value="email">Email</option>                            
+                            <option value="telefono">Telefono</option>
+                            <option value="cedula">Cedula</option>                            
+                            <option value="tipo">Tipo</option>  
+                        </Form.Control>
+                    </td>
+                </tr>
+            </tbody>
+          </Table>
+          
       <Table striped bordered hover>
         <thead>
           <tr>
